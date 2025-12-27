d6e12d5d7755206ce0850ce4fcd09712b9f322aa
Ricardo Martinez <ricardomartinez19b@gmail.com>
2025-12-26 18:06:19 -0600

    Use Vite splitVendorChunkPlugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,10 @@
-import { defineConfig } from "vite";
+import { defineConfig, splitVendorChunkPlugin } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), splitVendorChunkPlugin()],
   envPrefix: ["VITE_", "NEXT_PUBLIC_"],
   resolve: {
     dedupe: ["react", "react-dom", "react-router", "react-router-dom"],
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            if (
-              id.includes("/react/") ||
-              id.includes("/react-dom/") ||
-              id.includes("react/jsx-runtime")
-            ) {
-              return "react";
-            }
-            if (id.includes("@stackframe")) return "stackframe";
-            if (id.includes("@radix-ui") || id.includes("cmdk")) {
-              return "radix";
-            }
-            if (id.includes("react-router")) return "react-router";
-            return "vendor";
-          }
-          return undefined;
-        },
-      },
-    },
   },
 });
